e706c8435cb5c5839a1150207a876e0d73dbb9ed
Student User <student@workstation.lab.example.com>
2021-11-18 16:46:03 -0500

    blabla

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
-		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
+		echo "hello world";?> </p>
 	</body> 
 </html>
